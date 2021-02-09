030492daf9b07763e310a78b38fef46c313e5600
lvalente <valenleo313@gmail.com>
2021-02-09 16:34:11 -0400

    Second Commit

---
+++ src/Callendary.php
@@ -0,0 +1,16 @@
+<?php
+use Waller\Prueba;
+
+namespace Waller;
+
+class Callendary{
+
+    public function __construct()
+    {
+        $cal = new Callendary;
+    }
+
+    public function hoy(){
+        return "Hoy es:";
+    }
+}
---
+++ src/Prueba.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Waller;
+
+class Prueba{
+    public function prueba(){
+        return "prueba";
+    }
+}
